203be692c823cff0a31a4a1eab3288348df3428a
Zlatin Hristov <zlatin.hristov@softavis.com>
2025-09-14 23:36:23 +0300

    feat: customize log and cache directories for Vercel deployment

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,22 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function getLogDir(): string
+    {
+        if (!isset($_SERVER['VERCEL'])) {
+            return parent::getLogDir();
+        }
+
+        return '/tmp/logs/';
+    }
+
+    public function getCacheDir(): string
+    {
+        if (!isset($_SERVER['VERCEL'])) {
+            return parent::getCacheDir();
+        }
+
+        return '/tmp/cache/';
+    }
 }
